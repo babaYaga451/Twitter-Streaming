80477ac6f0cf3e9b8d1161d0d79d7038c7c40907
babaYaga451 <sambitkickers@gmail.com>
2021-07-06 21:29:05 +0530

    Introduced logging

--- function/main.py
+++ function/main.py
@@ -1,3 +1,11 @@
+from loguru import logger
+import base64
+
 
 def handle_request(event, context):
-    pass
+    logger.info("This Function was triggered by messageId {} published at {}",
+                context.event_id, context.timestamp)
+
+    if 'data' in event:
+        message = base64.b64decode(event['data']).decode('utf-8')
+        logger.info("Message recieved {}", message)
